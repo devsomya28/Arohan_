9feca1638df489b8295a6636c1ff77e32cc29dca
Somya Devda <devsomya@gmail.com>
2026-04-28 14:47:46 +0530

    deploy

--- backend/main.py
+++ backend/main.py
@@ -26,8 +26,8 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=False,
+    allow_origins=["https://chimerical-bombolone-f2d8b8.netlify.app", "http://localhost:3000"],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
